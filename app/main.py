ee401845e2e99a4146ab6ee03153c548b9be07dd
Bruno Perez <bruno178pm@gmail.com>
2020-06-16 11:15:42 -0700

    V1.0

--- app/main.py
+++ app/main.py
@@ -9,14 +9,14 @@ app.config['JSON_AS_ASCII'] = False
 
 estacionesTabla = {
     # idEstacion : [Nombre, Descr, Lat, Long, BtAddr]
-    204 : ["Estacion Barrio Parque","Estacion barrial situada en la casa de Bruno Perez", -31.433727, -64.213417, "bluetoohaddre"],
-    870 : ["Estación Buen Pastor" , "Estacion en el corazón de nueva cordoba",-31.423703, -64.186863 , "bt addre"]
+    204 : ["Estacion Barrio Parque","Estacion barrial situada en la casa de Bruno Perez", -31.433727, -64.213417, "B8:27:EB:92:C7:EF"],
+    870 : ["Estación Buen Pastor" , "Estacion en el corazón de nueva cordoba",-31.423703, -64.186863 , "B8:27:EB:92:C7:EF"]
 }
 
 userTabla = {
-    # idUser : [nombre, mail ,passwd, saldo]
-    500 : ["Bruno Perez", "bruno178pm@gmail.com" ,"abc123", 150],
-    600 : ["Lucas Pardina", "lucaspardina@gmail.com" ,"123abc", 150]
+    # idUser : [nombre, mail ,passwd, saldo, usandoBici]
+    500 : ["Bruno Perez", "bruno178pm@gmail.com" ,"abc123", 150, False],
+    600 : ["Lucas Pardina", "lucaspardina@gmail.com" ,"123abc", 150, False]
 }
 
 slotsTabla = {
@@ -50,33 +50,41 @@ def login():
         email = str(request.get_json()["email"])
         password = str(request.get_json()["password"])
 
-        for user in userTabla.values():
+        for userId in userTabla:
+            user = userTabla[userId]
             if (user[1] == email and user[2] == password):
-                return "ok"
+                return Response(str(userId), mimetype='text/plain')
             else:
-                return "Credenciales inválidas."
+                return Response("Credenciales inválidas.",  mimetype='text/plain')
         return email      
     
     else:
         abort(500, "No se envio un objeto json")
 
+@app.route("/user/<int:idUser>",methods=['GET'])
+def getUserInfo(idUser):
+    if (idUser in userTabla):
+        item = userTabla[idUser]
+        return {"nombre" : item[0], "saldo": item[3], "userId" : idUser, "isUsingBike" : item[4]}
 
 @app.route("/unlock",methods=['POST'])
-def unlock():
+def retirar():
     if (request.is_json):
         userId = (request.get_json()["userId"])
         slotId = (request.get_json()["slotId"])
         if (userId in userTabla and slotId in slotsTabla):
             if (userTabla[userId][3] > 0):
-                if (slotsTabla[slotId][1] == None):
-                    slotsTabla[slotId][1] = userId
-                    return "ok"
-                else:
-                    return "Slot ya ocupado"
+                    if (userTabla[userId][4]): #  Si la esta usando  
+                        slotsTabla[slotId][1] = None # Reseteo el slot
+                        userTabla[userId][3] -=50
+                    else:
+                        slotsTabla[slotId][1] = userId # No esta usando
+                    userTabla[userId][4] = not (userTabla[userId][4])
+                    return Response("ok",  mimetype='text/plain')
             else:
-                return "Saldo insuficiente."
+                return Response("Saldo insuficiente.",  mimetype='text/plain')
         else:
-            return "Usuario o bicicleta inexistente."
+            return Response("Usuario o bicicleta inexistente.",  mimetype='text/plain')
     else:
         abort(500, "No se envio un objeto json")
 
